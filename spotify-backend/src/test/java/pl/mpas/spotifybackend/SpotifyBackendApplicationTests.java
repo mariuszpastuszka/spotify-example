02bdf27fbd64c4ea54f107905df3a81f170b5555
maniek <maniek@dev.pl>
2019-11-21 13:40:36 +0100

    [init] - initial commit

---
+++ spotify-backend/src/test/java/pl/mpas/spotifybackend/SpotifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mpas.spotifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
